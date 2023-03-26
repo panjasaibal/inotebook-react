f8dee5151d4b41a27776b7af1a475e1a174235af
panjasaibal <panjasaibal1999@gmail.com>
2023-03-26 23:25:03 +0530

    added backend

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose= require('mongoose');
+
+
+const mongoUrl = "mongodb://127.0.0.1:27017/"
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoUrl).then(()=>{console.log("Connect succesfully")})
+}
+
+module.exports = connectToMongo;
