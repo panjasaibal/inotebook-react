3aed818eed366ef11f25fb48ea8424e1825bdaed
panjasaibal <panjasaibal1999@gmail.com>
2023-04-04 02:34:45 +0530

    Navbar and routes added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      Saibal
-    </div>
+    <Router>
+    
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        
+      </Routes>
+      
+
+    </Router>
   );
 }
 
