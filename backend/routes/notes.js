c9436524b40b36528dedacc5528135ce928ad612
panjasaibal <panjasaibal1999@gmail.com>
2023-04-04 01:39:08 +0530

    add delete note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -7,76 +7,98 @@ const { body, validationResult } = require('express-validator');
 
 // '/fetchAllNotes' get all notes of the user  :Login Required
 
-router.get('/fetchAllNotes',fetchUsers, async (req, res)=>{
+router.get('/fetchAllNotes', fetchUsers, async (req, res) => {
 
     try {
-        const notes = await Note.find({user:req.user.id});
-        res.json({notes});  
+        const notes = await Note.find({ user: req.user.id });
+        res.json({ notes });
     } catch (error) {
-        console.error(error.message); 
-       res.status(500).send('Internal server error');
+        console.error(error.message);
+        res.status(500).send('Internal server error');
     }
-    
+
 })
 
 
 // '/addNotes' get all notes of the user  :Login Required
 
-router.post('/addNotes',fetchUsers, [
-    body('tittle', 'Enter a valid tittle').isLength({min:3}),
-    body('description', 'Description cannot be null').isLength({min:5})
-],async (req, res)=>{
+router.post('/addNotes', fetchUsers, [
+    body('tittle', 'Enter a valid tittle').isLength({ min: 3 }),
+    body('description', 'Description cannot be null').isLength({ min: 5 })
+], async (req, res) => {
     //If any empty field occured then erro will be thrown
     const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array() })
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
     }
     try {
 
-        const {tittle, description, tag} = req.body;
-        const note = new Note({tittle, description, tag, user:req.user.id});
+        const { tittle, description, tag } = req.body;
+        const note = new Note({ tittle, description, tag, user: req.user.id });
         let savedNote = await note.save();
         res.json(savedNote);
-        
+
     } catch (error) {
-        console.error(error.message); 
+        console.error(error.message);
         res.status(500).send('Internal server error')
     }
 
-    
+
 })
 
 
 
-// '/updateNote' get all notes of the user  :Login Required
+// '/updateNote' update notes of the user  :Login Required
 
-router.put('/updateNote/:id',fetchUsers, async (req, res)=>{
-    
+router.put('/updateNote/:id', fetchUsers, async (req, res) => {
 
-    const {tittle, description, tag} = req.body;
-    const newNote ={};
 
-    if(tittle){newNote.tittle = tittle};
-    if(description){newNote.description = description};
-    if(tag){newNote.tag = tag};
+    const { tittle, description, tag } = req.body;
+    const newNote = {};
+
+    if (tittle) { newNote.tittle = tittle };
+    if (description) { newNote.description = description };
+    if (tag) { newNote.tag = tag };
 
     let note = await Note.findById(req.params.id);
-    if(!note){
-        res.status(404).send("Not Found !!!");
+    if (!note) {
+        return res.status(404).send("Not Found !!!");
     }
 
-    if(note.user.toString() != req.user.id){
-        res.status(401).send("Not alllowed !!");
+    if (note.user.toString() != req.user.id) {
+       return res.status(401).send("Not alllowed !!");
     }
 
-    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true});
-    res.json({note});
+    note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+    res.json({ note });
 
 })
 
 
 
+// '/deleteNote' delete notes of the user  :Login Required
 
+router.delete('/deleteNote/:id', fetchUsers, async (req, res) => {
+
+    try {
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+           return  res.status(404).send("Not Found !!!");
+        }
+
+        if (note.user.toString() != req.user.id) {
+            return res.status(401).send("Not alllowed !!");
+        }
+
+        note = await Note.findByIdAndDelete(req.params.id);
+        res.json({ Success: "Note has been deleted" });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Internal server error');
+    }
+
+
+})
 
 
 
